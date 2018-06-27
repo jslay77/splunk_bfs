e3f075aeab79b5f1cd7e35fc6053c067b338f5c4
jslay <jslay@splunk.com>
2018-06-27 15:36:37 -0400

    Quick add of networkx shit

---
+++ bin/nxbfs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, unicode_literals
+import app
+
+from splunklib.searchcommands import dispatch, ReportingCommand, Configuration, Option, validators
+import sys
+import networkx as nx
+
+
+@Configuration(requires_preop=True)
+class nxBfsCommand(ReportingCommand):
+    child = Option(
+        doc='''
+        **Syntax:** **child=***<fieldname>*
+        **Description:** Name of the field that will hold the computed sum''',
+        require=True, validate=validators.Fieldname())
+
+    parent = Option(
+        doc='''
+        **Syntax:** **parent=***<fieldname>*
+        **Description:** Name of the field that will hold the computed sum''',
+        require=True, validate=validators.Fieldname())
+
+    @Configuration()
+    def map(self, records):
+      child_field = self.child
+      parent_field = self.parent
+
+      children = []
+      parents = []
+
+      for record in records:
+        children.append(record[child_field])
+        parents.append(record[parent_field])
+      yield { 'children': children, 'parents': parents }
+
+    def reduce(self, records):
+      G = nx.Graph()
+      res = []
+      for r in records:
+        for num in range(len(r['children'])):
+          G.add_edge(r['children'][num], r['parents'][num])
+      
+      res.append([G.edges()])
+      #res.append(["foo", "bar"])
+
+     
+      yield { 'array': res }
+
+dispatch(nxBfsCommand, sys.argv, sys.stdin, sys.stdout, __name__)
