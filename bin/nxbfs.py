af9353c936ea8f0b273f9aef5cd0f0c5f66f53f6
jslay <jslay@splunk.com>
2018-06-28 11:06:38 -0400

    Kinda working as a Streaming command...

--- bin/nxbfs.py
+++ bin/nxbfs.py
@@ -2,27 +2,40 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import app
 
-from splunklib.searchcommands import dispatch, ReportingCommand, Configuration, Option, validators
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
 import sys
 import networkx as nx
 
 
-@Configuration(requires_preop=True)
-class nxBfsCommand(ReportingCommand):
+@Configuration()
+class nxBfsCommand(StreamingCommand):
     child = Option(
         doc='''
         **Syntax:** **child=***<fieldname>*
-        **Description:** Name of the field that will hold the computed sum''',
+        **Description:** Name of the field that holds the child''',
         require=True, validate=validators.Fieldname())
 
     parent = Option(
         doc='''
         **Syntax:** **parent=***<fieldname>*
-        **Description:** Name of the field that will hold the computed sum''',
+        **Description:** Name of the field that holds the parent''',
         require=True, validate=validators.Fieldname())
 
+    bfs_path = Option(
+        doc='''
+        **Syntax:** **parent=***<fieldname>*
+        **Description:** Name of the field that will hold the computed bfs_path''',
+        require=False, default="bfs_path", validate=validators.Fieldname())
+
+    bfs_count = Option(
+        doc='''
+        **Syntax:** **parent=***<fieldname>*
+        **Description:** Name of the field that will hold the computed bfs count''',
+        require=False, default="bfs_count", validate=validators.Fieldname())
+
     @Configuration()
-    def map(self, records):
+    def stream(self, records):
+      self.logger.debug('nxbfs: %s', self)
       c = self.child
       p = self.parent
       G = nx.Graph()
@@ -31,14 +44,9 @@ class nxBfsCommand(ReportingCommand):
       for r in records:
         G.add_edge(r[c], r[p])
         bfs=list(nx.bfs_tree(G, r[p])) 
-        res.append({r[p]: bfs})
-      yield { 'result': res }
-
-    def reduce(self, records):
-      res = []
-      for r in records:
-        for num in range(len(r['result'])):
-          res.append(r['result'][num])
-      yield { 'result': res }
+        r[self.bfs_path] = {r[p]: bfs}
+        r[self.bfs_count] = len(bfs)
+        yield r
+        
 
 dispatch(nxBfsCommand, sys.argv, sys.stdin, sys.stdout, __name__)
